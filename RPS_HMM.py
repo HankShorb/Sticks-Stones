8b494d60364acc8323c8890f40ab50b56af187c1
Hank <hankshorb@gmail.com>
2016-09-27 17:05:27 -0400

    Wee update

--- RPS_HMM.py
+++ RPS_HMM.py
@@ -1,6 +1,9 @@
 """Simple AI that plays rock paper scissors by modeling the user's sequential
 moves as a hidden markov model and making predictions on what the next move 
 will likely be"""
+
+#Hi, Andy
+
 import random
 
 # permanent int assignments for all three moves
