5cbdb3faaa664755da42a3effb72e78d93acebaa
Hank <hankshorb@gmail.com>
2016-09-27 17:09:19 -0400

    Delete RPS_HMM.py

--- RPS_HMM.py
+++
@@ -1,5 +0,0 @@
-"""Simple AI that plays rock paper scissors by modeling the user's sequential
-moves as a hidden markov model and making predictions on what the next move 
-will likely be"""
-
-#silly edit
